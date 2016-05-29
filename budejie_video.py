79aec70703469ab7089e793e4602761fae0612c3
heziliang <619237088@qq.com>
2016-05-29 23:12:22 +0800

    下载服务器视频到本地

---
+++ budejie_video.py
@@ -0,0 +1,9 @@
+#coding:utf-8
+import urllib
+import re
+def getVideo():
+	html = urllib.urlopen('http://www.budejie.com/video/').read()
+	pattern = re.compile(r'data-mp4="(.*?)"')
+	return re.findall(pattern,html)
+for line in getVideo():
+	urllib.urlretrieve(line,'video/ %s'%line.split('/')[-1])
