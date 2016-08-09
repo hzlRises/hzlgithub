3d6e79825311c910432fd0529e91c9a9daa7da50
heziliang <619237088@qq.com>
2016-08-09 14:03:14 +0800

    Update bzw315.py

--- bzw315.py
+++ bzw315.py
@@ -1,31 +1,20 @@
 #coding:utf-8
-from time import ctime
-import pycurl,re,StringIO,random
-
-def getHtml(url):
-	c = pycurl.Curl()
-	c.setopt(pycurl.CONNECTTIMEOUT, 60)
-	c.setopt(pycurl.TIMEOUT,120)	
-	c.setopt(pycurl.ENCODING, 'gzip,deflate')
-	c.setopt(pycurl.URL, url)
-	c.fp = StringIO.StringIO()
-	c.setopt(c.WRITEFUNCTION, c.fp.write)
-	c.perform()
-	html = c.fp.getvalue()
-	return html
-def getWord(html):
-	pattern = re.compile(r'<li><a.*?>(.*?)</a></li>')
-	word = pattern.findall(html)
-	return word
-print 'Begin:%s' %ctime()
-f = open('result.txt',r'w')
-url = ['http://baike.bzw315.com/zx/','http://baike.bzw315.com/sj/','http://baike.bzw315.com/cp/','http://baike.bzw315.com/pp/','http://baike.bzw315.com/sh/']
-for line in url:
-	html = getHtml(line)
-	word = getWord(html)
-	f.writelines(line+'\n' for line in word)
-f.close()
-print 'End:%s' %ctime()
-	
-	
-	
+import requests,sys,time
+reload(sys)
+sys.setdefaultencoding('utf-8')
+from bs4 import BeautifulSoup
+def getWord(url):
+	r = requests.get(url)	
+	s = BeautifulSoup(r.content,"lxml")	
+	for ul in s.find_all('ul',attrs={'class':'baike-list'}):
+		print ul.get_text().strip()
+		with open('result.txt',r'a+') as myfile:
+			myfile.write(ul.get_text().strip())
+def main():
+	start = time.clock()
+	url = ['http://baike.bzw315.com/zx/','http://baike.bzw315.com/sj/','http://baike.bzw315.com/cp/','http://baike.bzw315.com/pp/','http://baike.bzw315.com/sh/']
+	for url in url:		
+		getWord(url)
+	end = time.clock()
+	print 'RunTime: '+'%1.f s' %(end - start)
+main()
