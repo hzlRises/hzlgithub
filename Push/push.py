646a01d44878c96e1ca4154834d6111fcb197d08
heziliang <619237088@qq.com>
2016-08-09 15:43:00 +0800

    Update push.py

--- Push/push.py
+++ Push/push.py
@@ -1,8 +1,27 @@
 #coding:utf-8
-import requests
-#curl -H 'Content-Type:text/plain' --data-binary @urls.txt "http://data.zz.baidu.com/urls?site=domain&token=yourtoken&type=original" 
+import requests,pycurl,StringIO
+#curl
+curl -H 'Content-Type:text/plain' --data-binary @urls.txt "http://data.zz.baidu.com/urls?site=domain&token=yourtoken&type=original" 
+
+#requests
 headers = {'Content-Type':'text/plain'}
 url = 'http://data.zz.baidu.com/urls'
 params = {'site':'domain','token':'yourtoken','type':'original'}
-r = requests.post(url,params=params,headers=headers,data=open('urls.txt',r'rb'))
+r = requests.post(url,params=params,headers=headers,data=open('urls.txt',r'rb').read())
 print r.content
+
+#pycurl
+url = 'http://data.zz.baidu.com/urls?site=domain&token=yourtoken'
+c = pycurl.Curl()
+c.setopt(c.URL,url)
+c.setopt(pycurl.HTTPHEADER,['Content-Type:text/plain'])
+c.setopt(c.POST,1)
+c.setopt(c.POSTFIELDS,open('urls.txt',r'rb').read())
+b = StringIO.StringIO()
+c.setopt(c.WRITEFUNCTION, b.write)
+c.perform()
+info = b.getvalue()
+print info
+
+
+
