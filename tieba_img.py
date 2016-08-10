9974055eb0de322d147c2bba302b2c5b6db08a88
heziliang <619237088@qq.com>
2016-08-10 10:50:58 +0800

    Update tieba_img.py

--- tieba_img.py
+++ tieba_img.py
@@ -1,4 +1,31 @@
 #coding:utf-8
+author = 'heziliang'
+import requests,urllib,time
+from bs4 import BeautifulSoup
+def callback(blocknum, blocksize, totalsize):  
+    '''''回调函数 
+    @blocknum: 已经下载的数据块 
+    @blocksize: 数据块的大小 
+    @totalsize: 远程文件的大小 
+    '''  
+    percent = 100.0 * blocknum * blocksize / totalsize  
+    if percent > 100:  
+        percent = 100  
+    print "%.2f%%"% percent
+def getImg(url):	
+	r = requests.get(url)
+	s = BeautifulSoup(r.content,"lxml")
+	aTag = s.find('a',attrs={'class':'fl'})
+	imgTag = aTag.find('img')
+	print url
+	urllib.urlretrieve(imgTag.get('src'),'img/ %s' %imgTag.get('alt')+'.jpg',callback)
+def main():
+	for url in open('urls.txt'):	
+		getImg(url.strip())
+		time.sleep(1)
+main()
+
+'''
 import urllib
 import re
 
@@ -9,7 +36,7 @@ def imgGet():
 	return re.findall(reg,html)
 for line in imgGet():
 	urllib.urlretrieve(line,'img/ %s'%line.split('/')[-1])
-'''
+
 urllib.urlretrieve(url, filename, reporthook, data)
 参数说明：
 url：外部或者本地url
