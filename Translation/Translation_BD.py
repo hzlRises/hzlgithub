d908c20cd135adc42a3cff2b5b063fc26d540631
heziliang <619237088@qq.com>
2016-08-18 16:35:48 +0800

    Update Translation_BD.py

--- Translation/Translation_BD.py
+++ Translation/Translation_BD.py
@@ -1,10 +1,10 @@
-#coding=utf-8
+#coding:utf-8 
 _author_ = 'heziliang'
 import md5,random,requests,json,sys
 reload(sys)
 sys.setdefaultencoding("utf-8")
 def translate(q,lang):
-	appid = ''#自己申请id
+	appid = ''#自己申请的id
 	secretKey = '' #密钥
 	myurl = 'http://api.fanyi.baidu.com/api/trans/vip/translate'
 	fromLang = 'auto'
@@ -22,28 +22,32 @@ def translate(q,lang):
 		print jsondata["trans_result"][0]["dst"]		
 	except Exception,e:
 		print e
+def chiToeng():
+	while 1:
+		print 'Please Input Chinses: '
+		chiWord = raw_input()
+		if(chiWord == '0'):
+			engTochi()
+		translate(chiWord.decode('gbk').encode('utf-8'),'en')
+		print '------------------------------------------------------------'
+		print 'Input 0 To Reselect language'
+def engTochi():
+	while 1:
+		print 'Please Input English: ' 
+		engWord = raw_input()
+		if(engWord == '0'):
+			chiToeng()
+		translate(engWord.decode('gbk').encode('utf-8'),'zh')
+		print '------------------------------------------------------------'
+		print 'Input 0 To Reselect language'
 def main():
 	print 'Chinses2English Input 1, English2Chinses Input 2,Change Language Input 0'
 	while 1:				
-		cOre = raw_input('Please Input:1 or 2: ') 
-		if(cOre == '1'):
-			while 1:	
-				print 'Please Input Chinses: '
-				chiWord = raw_input()
-				if(chiWord == '0'):
-					break
-				translate(chiWord.decode('gbk').encode('utf-8'),'en')
-				print '------------------------------------------------------------'
-				print 'Input 0 To Reselect language'
-		elif(cOre == '2'):
-			while 1:
-				print 'Please Input English: ' 
-				engWord = raw_input()
-				if(engWord == '0'):
-					break
-				translate(engWord.decode('gbk').encode('utf-8'),'zh')
-				print '------------------------------------------------------------'
-				print 'Input 0 To Reselect language'
+		cOre = raw_input('Please Input:1 or 2: ')
+		if(cOre == '1'):				
+			chiToeng()
+		elif(cOre == '2'):			
+			engTochi()
 		elif(cOre == '0'):
 			pass
 		else:
