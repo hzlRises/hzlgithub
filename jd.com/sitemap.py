b462b3e1a1cbb560ffd7f2ccb2fed5c8a6ff4e08
二营长 <666@techseo.cn>
2016-12-24 14:07:55 +0800

    make sitemap

---
+++ jd.com/sitemap.py
@@ -0,0 +1,27 @@
+import os,datetime
+import sys
+reload(sys)
+sys.setdefaultencoding('utf-8')	
+
+item = 1
+f = open('item_%s.xml' % item, r'a+')
+  
+for i, line in enumerate(open('urls.txt')):
+	if i % 50000==0:
+		print i
+		f.write('<?xml version="1.0" encoding="utf-8"?>'+'\n')
+		f.write('<urlset>'+'\n')
+	f.write(' '+'<url>'+'\n')
+	f.write('  '+'<loc>'+line.strip()+'</loc>'+'\n')
+	f.write('  '+'<lastmod>'+str(datetime.date.today())+'</lastmod>'+'\n')
+	f.write('  '+'<changefreq>daily</changefreq>'+'\n')
+	f.write('  '+'<priority>1.0</priority>'+'\n')
+	f.write(' '+'</url>'+'\n')
+	if i % 50000==49999:
+		f.write('</urlset>')
+		f.close()
+		item += 1
+		f = open('item_%s.xml' % item, r'a+')
+f.write('</urlset>')
+f.close()
+
