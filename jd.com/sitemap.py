5224ba8d649b22bdf52d081368de6eccfbea8565
二营长 <666@techseo.cn>
2016-12-24 14:37:01 +0800

    Update sitemap.py

--- jd.com/sitemap.py
+++ jd.com/sitemap.py
@@ -1,12 +1,14 @@
-import os,datetime
+import datetime
 import sys
 reload(sys)
 sys.setdefaultencoding('utf-8')	
 author = 'heziliang'
 item = 1
 f = open('item_%s.xml' % item, r'a+')
-  
-for i, line in enumerate(open('urls.txt')):
+f_index = open('index_item.xml',r'a+')
+f_index.write('<?xml version="1.0" encoding="utf-8"?>'+'\n')
+f_index.write('<sitemapindex>'+'\n')
+for i, line in enumerate(open('urls.txt')):	
 	if i % 50000==0:
 		print i
 		f.write('<?xml version="1.0" encoding="utf-8"?>'+'\n')
@@ -18,10 +20,15 @@ for i, line in enumerate(open('urls.txt')):
 	f.write('  '+'<priority>1.0</priority>'+'\n')
 	f.write(' '+'</url>'+'\n')
 	if i % 50000==49999:
-		f.write('</urlset>')
+		f.write('</urlset>')		
 		f.close()
+		f_index.write(' '+'<sitemap>'+'\n')
+		f_index.write('  '+'<loc>item_%s.xml</loc>'%item+'\n')
+		f_index.write('  '+'<lastmod>'+str(datetime.date.today())+'</lastmod>'+'\n')
+		f_index.write(' '+'</sitemap>'+'\n')
 		item += 1
 		f = open('item_%s.xml' % item, r'a+')
 f.write('</urlset>')
 f.close()
-
+f_index.write('</sitemapindex>')
+f_index.close()
