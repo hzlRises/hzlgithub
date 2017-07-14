fe5ecc24463f2aee7ddc4b02863d50be266fe5a1
二营长 <666@techseo.cn>
2017-07-14 18:49:27 +0800

    Update cleaningkw.py

--- jd.com/cleaningkw.py
+++ jd.com/cleaningkw.py
@@ -30,3 +30,34 @@ def main():
 	
 if __name__ == '__main__':
 	main()
+'''
+#coding:utf-8
+#处理关键词特殊字符
+
+author = 'heziliang'
+import re
+def main():
+	for line in open('kw.txt'):
+		kw = line.strip()
+		kw = re.sub(r'(\(.*?\))','',kw)#两边都是英文括号
+		kw = re.sub(r'（.*?）','',kw)#两边都是中文括号
+		kw = re.sub(r'(\(.*?）)','',kw)#左边英文，右边中文
+		kw = re.sub(r'(（.*?\))','',kw)#左边中文，右边英文
+		kw = re.sub(r'>|<|\+|=|-|）|（|\)|\(|&|%|……|\^|￥|\$|#|@|】|【| |-|\.|\*|amp|nbsp|;|\/|\]|\[|{|}|：|；|》|《|、|:|。','',kw)#特殊字符
+		
+		reg6 = re.search(r'(\d+)$',kw)#数字结尾
+		reg7 = re.search(r'[a-zA-Z]+$',kw)#字母结尾
+		
+		if not reg6 or reg7:
+			kw = re.sub(r'\d+','',kw)#数字替换为空
+			kw = re.sub(r'[a-zA-Z]+','',kw)#字母替换为空
+			kw = kw.upper()#小写统一变大写
+			with open('results.txt',r'a+') as my:
+				my.write(kw+'\n')
+if __name__ == '__main__':
+	main()
+
+
+
+
+'''
