91b7ad3a237b04b21b4c526b979b4526a4218fdb
二营长 <admin@techseo.cn>
2017-08-15 12:20:59 +0800

    Update aa_chanpin.py

--- jd.com/aa_chanpin.py
+++ jd.com/aa_chanpin.py
@@ -286,3 +286,132 @@ if __name__ == '__main__':
 
 
 '''
+
+
+
+
+'''
+
+#coding:utf-8
+#优评内已启用关键词
+import requests,sys,time,re
+from bs4 import BeautifulSoup
+import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+from email.header import Header
+from email.mime.application import MIMEApplication
+from email.mime.base import MIMEBase
+from email import Encoders
+reload(sys)
+sys.setdefaultencoding('utf8')
+author = 'heziliang'
+headers = {
+    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
+    "Accept-Encoding": "gzip, deflate, sdch",
+    "Accept-Language": "zh-CN,zh;q=0.8",
+    "Connection": "keep-alive",
+    "Cookie": "mx=0_X; ipLocation=%u5317%u4eac; ipLoc-djd=1-2810-51081-0.138453223; cn=0; mt_xid=V2_52007VwMXWltaUlMfSxleAmIGFVFYWVRSF0EpWVJnAxBRWg1OXR5JHEAAZgMXTg5QUVwDSEpYBTUCQFJfCFMPL0oYXAN7AhtOXFBDWhlCGFsOZQYiUG1YYlofQB5ZAmIHG2JdXVRd; xtest=8133.cf6b6759; TrackID=1T5EuXf-GW_nDMjms6zHLILae2dn9RBjOPC_agJqDSzgjFtyHXYKeXnmOV4FNJXverDsCGtCxweO7L08iiSfIkvAweT9ycz2QmzCM8XZmrro; pinId=sEXqQzc6jbTC4-ywFcvXDrV9-x-f3wj7; unick=jd_188001lin; _tp=tAk%2BinB8QxVDni0qm6hl9DXhbd5sz3LXsCXtaG4AeX4%3D; _pst=jd_51a61be6b7e94; dmpjs=dmp-d217391dbf70cd8b8362421055cf0d288074c0c; __jdv=122270672|jd.com|dmp_1528|cpc|dmp_1528_80702_d217391dbf70cd8b8362421055cf0d288074c0c_1502421221|1502421222581; __jda=122270672.1497318131364462407999.1497318131.1499689625.1499853526.6; __jdb=122270672.12.1497318131364462407999|6.1499853526; __jdc=122270672; __jdu=1497318131364462407999; 3AB9D23F7A4B3C9B=SRFBEFEUQOW6A6HQK43UV4IIDN2F3ZNPV2LWJPBJJWDCACNBBMNYPY7ZYPLYE43ZEQGT7BO74O3V3VOUNSDW3C5KPI",
+    "Host": "search.jd.com",
+	"Referer":"https://www.jd.com/",
+    "Upgrade-Insecure-Requests": "1",
+    "User-Agent": "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.115 Safari/537.36",
+}
+successNUm = 0#成功数
+failNum = 0#失败数
+re_num = 36#起始result后缀
+def getCountnum(i,kw):#文件后缀、关键词
+	global successNUm
+	global failNum
+	global re_num
+	keyw = kw.split(',')[0].replace('"','')#只取组合后关键词
+
+	if (successNUm + failNum)%50000 == 49999:
+		time.sleep(60)#处理完一个文件暂停30秒
+		sendMail(re_num)
+		re_num += 1
+		
+	#请求搜索
+	try:
+		url = 'https://search.jd.com/Search?keyword=%s&enc=utf-8'%keyw
+		r = requests.get(url,headers=headers,timeout=60)
+		s = BeautifulSoup(r.content,'lxml')
+		resCount = s.find('span',attrs={'id':'J_resCount'}).get_text()
+		if '+' in str(resCount):
+			resCount = resCount.replace('+','')
+		with open('result_%s.txt'%re_num,r'a+') as my:#结果存放文件
+			my.write(keyw+','+resCount+'\n')
+		successNUm += 1
+	except Exception,e:
+		with open('fail_%s.txt'%re_num,r'a+') as my:#异常关键词文件
+			my.write(keyw+'\n')
+		failNum += 1
+		print e 
+	print 'successNUm,failNum:'+str(successNUm)+','+str(failNum)
+	
+mail_host = 'smtp.163.com'
+mail_user = 'hzl_789789@163.com'
+mail_pass = '201508241653hzl'
+mail_to = 'heziliang@jd.com'
+
+#发送附件邮件
+def sendMail(rank):
+	try:
+		msg = MIMEMultipart()
+		msg['Subject'] = Header(u'优评-运营添加-启用词- %s'%rank,'utf-8')  #主题
+		msg['From'] = mail_user#
+		msg['To'] = mail_to#
+	#	msg.attch(MIMEText(u'品牌+三级分词- %s of 36'%rank,'plain','utf-8'))
+	#	with open('result_%s.txt'%rank,'rb') as f:
+	#		mime = MIMEBase('file','txt',filename='result_%s.txt'%rank)
+	#		mime.add_header('Content-Disposition', 'attachment', filename='result_%s.txt'%rank)
+	#		mime.set_payload(f.read())
+	#		Encoders.encode_base64(mime)
+	#		msg.attach(mime)
+		partSuccess = MIMEApplication(open('result_%s.txt'%rank,'rb').read())
+		partSuccess.add_header('Content-Disposition','attachment',filename='result_%s.txt'%rank)
+		msg.attach(partSuccess)
+		
+#		partFail = MIMEApplication(open('fail_%s.txt'%rank,'rb').read())
+#		partFail.add_header('Content-Disposition','attachment',filename='fail_%s.txt'%rank)
+#		msg.attach(partFail)
+	except Exception,e:
+		print e
+		#发送邮件
+	try:
+		s = smtplib.SMTP()
+		s.connect(mail_host)
+		s.login(mail_user, mail_pass)
+		s.sendmail(mail_user,mail_to, msg.as_string())
+		s.close()
+	except Exception,e:
+		print 'sendMail failed...'
+		print e
+
+def main():
+	for i in range(5,10):
+		for line in open('%s.csv'%i):#
+			kw = line.strip()
+			#处理字符
+			reg = re.compile(r'(\(.*?\))')
+			reg2 = re.compile(r'（.*?）')
+			reg3 = re.compile(r'(\(.*?）)')
+			reg4 = re.compile(r'(（.*?\))')
+			reg5 = re.compile(r'\&')
+			kw = re.sub(reg,'',kw)#两边都是英文括号
+			kw = re.sub(reg2,'',kw)#两边都是中文括号
+			kw = re.sub(reg3,'',kw)#左边英文，右边中文
+			kw = re.sub(reg4,'',kw)#左边中文，右边英文
+			kw = re.sub(reg5,'',kw)#&符号
+			kw = kw.upper()#小写变大写
+			kw = kw.replace(' ','')#空格
+			getCountnum(i,kw)
+			time.sleep(0.1)
+if __name__ == '__main__':
+	main()
+
+
+'''
+
+
+
