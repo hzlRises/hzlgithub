0f5a6ecd841f3c3cff872ae88553137fdffbb388
二营长 <admin@techseo.cn>
2017-09-19 18:00:55 +0800

    Update jd_zhishi.py

--- Push/jd_zhishi.py
+++ Push/jd_zhishi.py
@@ -8,6 +8,78 @@ sys.setdefaultencoding('utf-8')
 
 
 def main():
+#	删掉yesterday文件
+#	if os.path.exists('yesterday.txt'):
+#		os.remove('yesterday.txt')
+
+	
+	#把xml中的数据拿下来，并和现有的数据去重后，留下的数据单独放到一个文件，并且追加到所有的url txt里
+	url = 'http://yp.jd.com/22/22.xml'
+	
+	r = requests.get(url)
+	zhishi_url = re.findall(r'<loc>(.*?)</loc>',r.content)
+	has_push_list = [url.strip() for url in open('all_url.txt')]
+	f = open('all_url.txt',r'a+')
+	f_ytd = open('yesterday.txt',r'w+')
+	
+	num = 0
+	txt_index = 0
+	for link in zhishi_url:#多
+		if link in has_push_list:
+			pass
+		else:
+			f.write(link+'\n')#追加到所有的url txt里
+			f_ytd.write(link+'\n')
+			#把还未推送的url放到单独的文件内
+			if num%2000 == 1999:
+				f_ytd.close()
+				txt_index += 1
+				f_ytd = open('yesterday_%s.txt'%txt_index,r'w+')
+			num += 1
+				
+	f.close()
+	f_ytd.close()
+	print 'yesterday has %s'%num
+	print 'crawl done'
+	
+	time.sleep(600)
+
+	#开始推送
+	print 'push begin'
+	for i in range(0,txt_index+1):
+		try:
+			headers = {'Content-Type':'text/plain'}
+			url = 'http://data.zz.baidu.com/urls'
+			params = {'site':'www.jd.com','token':'2222222'}#,'type':'original'
+			r = requests.post(url,params=params,headers=headers,data=open('yesterday_%s.txt'%i,r'rb').read())
+			print r.content
+		except Exception,e:
+			print e
+			continue
+	print 'Finish!!!'
+
+if __name__ == '__main__':
+	main()
+
+
+
+
+
+
+
+
+
+'''
+#coding:utf-8
+import requests,time,re,os
+import sys
+reload(sys)
+sys.setdefaultencoding('utf-8')
+
+
+
+
+def main():
 	#删掉yesterday文件
 	if os.path.exists('yesterday.txt'):
 		os.remove('yesterday.txt')
@@ -68,5 +140,5 @@ if __name__ == '__main__':
 
 
 
-
+'''
 
