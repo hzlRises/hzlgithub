d0b6b103491ed355bdbd295c0a18ca6448d2b8cc
二营长 <admin@techseo.cn>
2018-01-09 19:49:20 +0800

    Update baidu-aip.py

--- imagerecognition/baidu-aip.py
+++ imagerecognition/baidu-aip.py
@@ -2,6 +2,11 @@
 from aip import AipOcr
 import json
 '''你的 APPID AK SK '''
+'''
+https://cloud.baidu.com/doc/OCR/OCR-Python-SDK.html#.E9.80.9A.E7.94.A8.E6.96.87.E5.AD.97.E8.AF.86.E5.88.AB
+
+'''
+
 APP_ID = '***'
 API_KEY = '***'
 SECRET_KEY = '***'
