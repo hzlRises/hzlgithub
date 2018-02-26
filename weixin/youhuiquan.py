e1765cdf2f0d8b2e3b559e881d6d3952bbe2c03f
二营长 <admin@techseo.cn>
2018-02-26 22:26:37 +0800

    Update youhuiquan.py

--- weixin/youhuiquan.py
+++ weixin/youhuiquan.py
@@ -19,19 +19,43 @@ def getShortUrl(url):
 # 注册消息响应事件，消息类型为itchat.content.TEXT，即文本消息
 def text_reply(msg):
 	print msg['Text']#unicode
-	print type(msg['Text'])#unicode
+#	print type(msg['Text'])#unicode
 #	msg['Text'].encode("utf-8")  unicode转为 str
 #	print urllib.quote(msg['Text'].encode("utf-8"))	
 	if 'jd.com' in msg['Text']:
 		sku_id = jd.getGoodsIdByUrl(msg['Text'])
 		goods_name,price,fanli = jd.getProductInfo(sku_id)
 		click_url = jd.getFanliLink(sku_id)		
-		message = u'一一一一返 利 信 息一一一一\n'+goods_name+'\n'+u'【商品原价】'+price+'\n'+u'【商品返利】'+fanli+'\n'+u'【返利链接】'+getShortUrl(click_url)
+		message = u'一一一一返 利 信 息一一一一\n'+goods_name+'\n'+u'【商品原价】'+price+'元'+'\n'+u'【商品返利】'+fanli+'元'+'\n'+u'【返利链接】'+getShortUrl(click_url)
 	elif '帮助' in msg['Text']:
-		message = '【在淘宝购物前领券】假如你想买鼠标垫，直接把鼠标垫三个字发给机器人，机器人会把找到的鼠标垫相关商品链接发给你，点进去复制淘口令，再打开淘宝/天猫APP就可以领到优惠券啦\n【在京东购物】打开京东APP，找到自己想买的商品，点击右上角分享按钮，把商品链接复制，发给我就可以看到返利消息啦\n快试试吧...'
+		message = '[愉快]【在淘宝购物前领券】假如你想买鼠标垫，直接把鼠标垫三个字发给机器人，机器人会把找到的鼠标垫相关商品链接发给你，点进去复制淘口令，再打开淘宝/天猫APP就可以领到优惠券啦。\n[愉快]【在京东购物】打开京东APP，找到自己想买的商品，点击右上角分享按钮，把商品链接复制，发给我就可以看到返利链接啦，确认收货后发送【提现】，等待客服审核就可以啦。\n快试试吧...'
+	elif '提现' in msg['Text']:	
+	
+		#需要给主人发消息，先获得主人的id，以@符号开头
+		user_content = itchat.search_friends(name = u'雨一直下')
+		
+		userName = user_content[0]['UserName']
+		
+		#获取提现申请人的昵称
+		friend_content = itchat.search_friends(userName = msg.fromUserName)
+		print friend_content
+		friend_name = friend_content['NickName']
+
+		#给主人发消息提醒
+		itchat.send( friend_name+u'申请提现',toUserName = userName)		
+			
+		#判断提现人性别
+		if friend_content['Sex'] == 1:
+			message = friend_name +u'帅哥，请耐心等待哦，由于人数较多，客服火速确认中呢...'		
+		elif friend_content['Sex'] == 0:
+			message = friend_name +u'美女，请耐心等待哦，由于人数较多，客服火速确认中呢...'
+		else:
+			message = friend_name +u'美女，请耐心等待哦，由于人数较多，客服火速确认中呢...'
+			
+			
 	else:
 		url = 'http://yhq.techseo.cn/yhq/?r=l&kw=%s'%(urllib.quote(msg['Text'].encode("utf-8")))
-		message = u'一一一一导 购 信 息一一一一\n已为您找到:%s\n点击下方链接查看\n%s\n-----------\n发送【帮助】查看使用机器人流程'%(msg['Text'],getShortUrl(url))
+		message = u'一一一一导 购 信 息一一一一\n已为您找到:%s\n点击下方链接查看\n%s\n-----------\n发送【帮助】查看使用机器人流程\n更多大额神券商品点击下方链接：\nhttp://t.cn/RTzLM6g'%(msg['Text'],getShortUrl(url))
 
 	print message
 	itchat.send(message,msg.fromUserName)
@@ -56,7 +80,7 @@ def add_friend(msg):
     # 该操作会自动将新好友的消息录入，不需要重载通讯录
     itchat.add_friend(**msg['Text'])
     # 加完好友后，给好友打个招呼
-    itchat.send_msg('你好，我是微信自动回复消息机器人,想在淘宝/天猫买东西？直接把关键词发给我，机器人帮你找相关商品优惠券。\n想在京东买东西？把链接发给我，机器人给你发返利红包，发送【帮助】查看使用机器人流程。', msg['RecommendInfo']['UserName'])
+    itchat.send_msg('[愉快]你好，我是微信自动回复消息机器人\n[抠鼻]想在淘宝/天猫买东西？直接把关键词发给我，机器人帮你找相关商品优惠券。\n[抠鼻]想在京东买东西？把链接发给我，机器人给你发返利红包\n--------------\n发送【帮助】查看使用机器人流程。', msg['RecommendInfo']['UserName'])
 	
 		
 
