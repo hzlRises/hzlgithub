2dfcd3b4ca856810e991ed4ff99223f448978c38
二营长 <admin@techseo.cn>
2018-03-27 14:43:34 +0800

    Update push.py

--- tea/push.py
+++ tea/push.py
@@ -62,35 +62,20 @@ for i in range(0,txt_index+1):
 	time.sleep(1)
 	#主动推送
 	
-	params_zd = {'site':'heziliang.cn','token':'Did6Hw3NtVVJxzYg'}
-	r_zd = requests.post(url,params=params_zd,headers=headers,data=open('yesterday_%s.txt'%i,r'rb').read())
-	
-	
-	
-	#mip
-	params = {'site':'heziliang.cn','token':'Did6Hw3NtVVJxzYg','type':'mip'}
-	r = requests.post(url,params=params,headers=headers,data=open('yesterday_%s.txt'%i,r'rb').read())
-	
-	
-	#amp
-	params_m = {'site':'heziliang.cn','token':'Did6Hw3NtVVJxzYg','type':'amp'}
-	r_m = requests.post(url,params=params_m,headers=headers,data=open('yesterday_%s.txt'%i,r'rb').read())
-	
-
-	print 'zd_push:'+r_zd.content
-	print 'mip_push:'+r.content
-	print 'amp_push:'+r_m.content
-
-
-'''
-#熊掌号
-params_xzh = {'appid':'1584638868980905','token':'99XYR1EO50dhybGu','type':'realtime'}
-r_xzh = requests.post(url,params=params_xzh,headers=headers,data=open('yesterday_m_%s.txt'%i,r'rb').read())
-
-
-UPDATE `mip_articles` SET `publish_time`=1521811786  WHERE `id` > 9824
-UPDATE `mip_articles` SET `cid` = 13 WHERE  id > 9824 and id < 13824
-
-'''
-
+	for link in open('yesterday_%s.txt'%i):
+		params_zd = {'site':'heziliang.cn','token':'Did6Hw3NtVVJxzYg'}
+		r_zd = requests.post(url,params=params_zd,headers=headers,data=link.strip())
+		
+		#mip
+		params = {'site':'heziliang.cn','token':'Did6Hw3NtVVJxzYg','type':'mip'}
+		r = requests.post(url,params=params,headers=headers,data=link.strip())
+		
+		#amp
+		params_m = {'site':'heziliang.cn','token':'Did6Hw3NtVVJxzYg','type':'amp'}
+		r_m = requests.post(url,params=params_m,headers=headers,data=link.strip())
+		print 'zd_push:'+r_zd.content
+		print 'mip_push:'+r.content
+		print 'amp_push:'+r_m.content
+		time.sleep(10)
+		
 
