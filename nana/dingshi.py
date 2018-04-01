63c701bf91306cb2151356a3cdc2991c91f7dc1e
二营长 <admin@techseo.cn>
2018-04-01 17:06:50 +0800

    Update dingshi.py

--- nana/dingshi.py
+++ nana/dingshi.py
@@ -5,3 +5,13 @@ while True:
 		print "Hello World"
 		os.system("python go.py")
     time.sleep(1)
+'''
+print current_time//time.localtime(time.time())
+
+time.struct_time(tm_year=2018, tm_mon=4, tm_mday=1, tm_hour=16, tm_min=55, tm_sec=38, tm_wday=6, tm_yday=91, tm_isdst=0)
+
+
+
+time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time()))
+
+'''
