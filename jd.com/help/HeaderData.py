ec939f2b2acb5bb9f97c8f86616926182c13fe00
二营长 <admin@techseo.cn>
2018-04-10 19:32:01 +0800

    Create HeaderData.py

---
+++ jd.com/help/HeaderData.py
@@ -0,0 +1,20 @@
+#coding:utf-8
+def get_header():
+	headers = {
+    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
+    "Accept-Encoding": "gzip, deflate",
+    "Accept-Language": "zh-CN,zh;q=0.8",
+    "Cache-Control": "no-cache",
+    "Connection": "keep-alive",
+    "Cookie": '''''',
+    "Host": "help.jd.com",
+    "Pragma": "no-cache",
+    "Referer": "http://help.jd.com/user/issue/list-28.html",
+    "Upgrade-Insecure-Requests": "1",
+    "User-Agent": "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36",    
+	}
+	return headers
+def main():
+	pass
+if __name__ == '__main__':
+	main()
