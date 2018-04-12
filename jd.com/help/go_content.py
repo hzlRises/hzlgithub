77d8231271647d9e95f3bbb0b6f492f488c5bbd9
二营长 <admin@techseo.cn>
2018-04-12 20:28:41 +0800

    Update go_content.py

--- jd.com/help/go_content.py
+++ jd.com/help/go_content.py
@@ -83,3 +83,107 @@ def main():
 	
 if __name__ == '__main__':
 	main()
+
+	
+	
+	
+	
+'''
+#coding:utf-8
+import requests,sys,re,pycurl,StringIO,time,xlrd,xlwt,HeaderData,md5
+from bs4 import BeautifulSoup
+reload(sys) 
+sys.setdefaultencoding('utf8')
+
+def main():	
+	wb = xlwt.Workbook()
+	sheet = wb.add_sheet('sheet1')
+	sheet.write(0,0,'categoryID')#categoryID帮助中心
+	sheet.write(0,1,'status')#status未审核
+	sheet.write(0,2,'recommend')#recommend未推荐
+	sheet.write(0,3,'type')#type运营添加
+	sheet.write(0,4,'tag')#tag标签
+	sheet.write(0,5,'source')#文章来源
+	sheet.write(0,6,'writer')#作者
+	sheet.write(0,7,'md5_id')
+	sheet.write(0,8,'title')
+	sheet.write(0,9,'description')
+	sheet.write(0,10,'content')
+			
+	content_url_list = [url.strip() for url in open('jrhelp.jd.com_index_detail_url.txt')]
+	for index,k in enumerate(content_url_list):		
+		title = ''
+		try:
+			url = k#获取文件中的url，具体根据txt里字段定
+			print index,url
+		except Exception,e:
+			print e
+		
+		#抓正文规则
+		try:
+			r = requests.get(url,headers=HeaderData.get_header(),timeout=60)
+			s = BeautifulSoup(r.content,"lxml")
+			
+			title = str(s.find('title').get_text()).split('-')[0].decode('utf-8')
+			content = str(s.find('div',attrs={"class":"wanecon"}))
+			#匹配所有字符\s匹配任意的空白符（包括空格，制表符(Tab)，换行符，中文全角空格）\S则是任意不是空白符的字符
+			content = re.sub(r'<div align=[\s\S]*','',content).decode('utf-8')
+			
+			'''
+			content = str(s.find('div',attrs={"class":"contxt"}))			
+			content_str = re.sub('<div class="help-tit1 flk06">(.*?)</div>','',content).decode('utf-8')			
+			
+			s2 = BeautifulSoup(content_str,"lxml")#把content_str当做html再用BeautifulSoup解析一次
+			
+			#content_txt = str(s.find('div',attrs={"class":"contxt"}).get_text()).decode('utf-8')[0:250]#取前250个字符
+			'''
+			s2 = BeautifulSoup(content,"xml")
+			content_txt = s2.get_text()[0:250]#取前250个字符
+			
+			#替换掉引号和空格、换行等，英文引号会影响描述标签，空格、换行等会影响json里的答案字段
+			content_txt = re.sub(r'[\s\'\"]*?','',content_txt)
+			
+		
+				
+			
+			m1 = md5.new()
+			m1.update(title)			
+			md5_str = m1.hexdigest()[8:-8]#取中间16位
+			
+			
+			sheet.write(index+1,0,'148')#categoryID帮助中心
+			sheet.write(index+1,1,'1')#status未审核
+			sheet.write(index+1,2,'1')#recommend未推荐
+			sheet.write(index+1,3,'0')#type运营添加
+			sheet.write(index+1,4,'jd')#tag标签
+			sheet.write(index+1,5,'jd')#文章来源
+			sheet.write(index+1,6,'jd')#作者
+			sheet.write(index+1,7,md5_str)
+			sheet.write(index+1,8,title)				
+			if content_txt:
+				sheet.write(index+1,9,content_txt)
+			else:
+				sheet.write(index+1,9,title)
+				
+			if len(content) < 32767:
+				sheet.write(index+1,10,content+'</div>')
+			else:
+				sheet.write(index+1,10,'String longer than 32767 characters')
+			wb.save("result.xls")
+			
+			
+			'''
+			content = [soup.extract() for soup in s('img')]#删除文章里的img标签
+			content = [soup.extract() for soup in s('a')]#删除文章里的img标签
+			content = str(s.find('td')).replace('<td>','').replace('</td>','').replace('div','p')#替换掉td和div	
+			'''	
+		except Exception,e:
+			print e
+			
+			
+		time.sleep(0.1)		
+		
+if __name__ == '__main__':
+	main()
+
+'''
