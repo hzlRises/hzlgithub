a986f42a951dbe8ad89d0345d1aca140e9210c84
二营长 <admin@techseo.cn>
2018-04-13 09:58:54 +0800

    Update push.py

--- Push/push.py
+++ Push/push.py
@@ -1,27 +1,52 @@
 #coding:utf-8
-import requests,pycurl,StringIO
-#curl
-curl -H 'Content-Type:text/plain' --data-binary @urls.txt "http://data.zz.baidu.com/urls?site=domain&token=yourtoken&type=original" 
+import requests,time
+import sys
+reload(sys)
+sys.setdefaultencoding('utf-8')
+def main():
+	file_name = 'seo_kw_urls_2018_04_12'
+	
+	
+	part = 0
+	
+	f = open('part_%s.txt'%part,r'w+')	
+	for i,line in enumerate(open('%s.txt'%file_name)):
+		try:			
+			f.write(line.strip().split(',')[1]+'\n')
+		except Exception,e:
+			print e
+		if i%2000 == 1999:
+			f.close()
+			part += 1
+			f = open('part_%s.txt'%part,r'w+')
+	f.close()
 
-#requests
-headers = {'Content-Type':'text/plain'}
-url = 'http://data.zz.baidu.com/urls'
-params = {'site':'domain','token':'yourtoken','type':'original'}
-r = requests.post(url,params=params,headers=headers,data=open('urls.txt',r'rb').read())
-print r.content
+	
+	print part
+	time.sleep(3)
+	
+	count = 0
+	
+	for i in range(0,part+1):
+		try:
+			headers = {'Content-Type':'text/plain'}
+			url = 'http://data.zz.baidu.com/urls'
+			params = {'site':'club.jd.com','token':'00Mteo2F4brQRR6S'}#,'type':'original'
+			r = requests.post(url,params=params,headers=headers,data=open('part_%s.txt'%i,r'rb').read())
+			print r.content
+		except Exception,e:
+			print e
+			continue
+		
+		count += 1
+		print count
+		time.sleep(3)
+		
+		
+	print 'The data push is complete'
 
-#pycurl
-url = 'http://data.zz.baidu.com/urls?site=domain&token=yourtoken'
-c = pycurl.Curl()
-c.setopt(c.URL,url)
-c.setopt(pycurl.HTTPHEADER,['Content-Type:text/plain'])
-c.setopt(c.POST,1)
-c.setopt(c.POSTFIELDS,open('urls.txt',r'rb').read())
-b = StringIO.StringIO()
-c.setopt(c.WRITEFUNCTION, b.write)
-c.perform()
-info = b.getvalue()
-print info
+if __name__ == '__main__':
+	main()
 
 
 
