efad013a451798c2360004670ba83f002ff2d19f
二营长 <admin@techseo.cn>
2018-04-16 21:41:29 +0800

    Create HeaderData.py

---
+++ jd.com/meilele/HeaderData.py
@@ -0,0 +1,19 @@
+#coding:utf-8
+def get_header():
+	headers = {
+    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
+    "Accept-Encoding": "gzip, deflate",
+    "Accept-Language": "zh-CN,zh;q=0.8",
+    "Cache-Control": "no-cache",
+    "Connection": "keep-alive",
+    "Cookie": '''aliyungf_tc=AQAAAPdkVFXUsAUAfC3Ab40xkRJJBMKX; MA_si=oc1jg28v8wr_3883461; MLL_CID=zatjg28v8wr_3883461; _SC_=1; ECS_ID=j202386c9847c227c0f6c973a8bd05b92d31e5b9; _o=0; currentCity=%E5%8C%97%E4%BA%AC; region_id_start=35; region_pinyin=beijing; region_name=%E5%8C%97%E4%BA%AC; region_id=35; hhyh_page_version=2; _gscu_270546881=23883463lj5uhb11; _gscbrs_270546881=1; AD_center=1; __utma=184173242.2127987443.1523883578.1523883578.1523883578.1; __utmc=184173242; __utmz=184173242.1523883578.1.1.utmcsr=(direct)|utmccn=(direct)|utmcmd=(none); __utmt=1; Hm_lvt_d91942b1e6dd95baed4560c0c6d8071b=1523883578; pgv_pvi=9934447616; pgv_si=s489232384; _jzqa=1.4466675477914213400.1523883579.1523883579.1523883579.1; _jzqc=1; _jzqckmp=1; _qzjc=1; _SD_=5; _i=5; countView=5; _gscs_270546881=23883463ybueu611|pv:5; __utmb=184173242.4.10.1523883578; Hm_lpvt_d91942b1e6dd95baed4560c0c6d8071b=1523883758; _qzja=1.441984976.1523883579335.1523883579335.1523883579335.1523883627332.1523883757739.0.0.0.4.1; _qzjb=1.1523883579335.4.0.0.0; _qzjto=4.1.0; _jzqb=1.4.10.1523883579.1''',
+    "Host": "www.meilele.com",
+    "Pragma": "no-cache",
+    "Upgrade-Insecure-Requests": "1",
+    "User-Agent": "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36",    
+	}
+	return headers
+def main():
+	pass
+if __name__ == '__main__':
+	main()
