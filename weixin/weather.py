c8d5183f8ce37ec57f9097a4aadd6153919bf750
二营长 <admin@techseo.cn>
2018-04-18 21:04:39 +0800

    Update weather.py

--- weixin/weather.py
+++ weixin/weather.py
@@ -8,13 +8,13 @@ reload(sys)
 sys.setdefaultencoding('utf-8')	
 
 def save_jpg():
-	city_list = ['beijingtianqi','chengdutianqi','handantianqi','xiamentianqi']		
+	city_list = ['xiamentianqi','beijingtianqi']
 	for index,city in enumerate(city_list):
 		time.sleep(1)
 		encode_city = quote(city)
 		url = 'https://www.baidu.com/s?ie=UTF-8&wd=%s'%encode_city	
 		picName = '%s.png'%index		
-		browser = webdriver.PhantomJS()
+		browser = webdriver.PhantomJS(executable_path=r'D:\programfiles\anaconda\Lib\site-packages\selenium\webdriver\phantomjs\bin\phantomjs.exe')
 		browser.get(url)
 		browser.maximize_window()
 		browser.save_screenshot(picName)#保存截图		
@@ -28,7 +28,7 @@ def save_jpg():
 		i = Image.open(picName)	#打开0.png
 		tinaqi = i.crop(rangle)#使用Image的crop函数，从截图中再次截取我们需要的		
 		tinaqi.save('send_%s.png'%index)		
-		browser.close()		
+		browser.close()	
 		
 		
 		#发送
@@ -36,9 +36,10 @@ def save_jpg():
 		userName = user_content[0]['UserName']
 		itchat.send_image('send_%s.png'%index,toUserName = userName)
 		
-		user_content_baby = itchat.search_friends(name=u'')
-		userName_baby = user_content_baby[0]['UserName']
-		itchat.send_image('send_%s.png'%index,toUserName = userName_baby)
+		if city == 'xiamentianqi':
+			user_content_baby = itchat.search_friends(name=u'徐莹')
+			userName_baby = user_content_baby[0]['UserName']
+			itchat.send_image('send_%s.png'%index,toUserName = userName_baby)
 		
 
 	
@@ -48,76 +49,10 @@ def main():
 		time.sleep(1)
 		current_time = time.localtime(time.time())
 		print time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time()))	
-		if ((current_time.tm_hour == 7) and (current_time.tm_min) == 30 and (current_time.tm_sec == 0)):
+		if ((current_time.tm_hour == 7) and (current_time.tm_min) == 0 and (current_time.tm_sec == 0)):
 			save_jpg()
 
 
 if __name__ == '__main__':
 	main()
 
-
-
-
-
-'''
-#coding:utf-8
-import itchat,json,time
-import requests,sys
-reload(sys) 
-sys.setdefaultencoding('utf8')
-
-def getWeather(cs):
-	url = 'http://wthrcdn.etouch.cn/weather_mini?citykey=%s'%cs
-	r = requests.get(url)
-	j_data = json.loads(r.content)
-	for i in range(1):
-		city = j_data["data"]["city"]
-		aqi = j_data["data"]["aqi"]#空气质量指数
-		ganmao = j_data["data"]["ganmao"]
-		wendu = j_data["data"]["wendu"]
-
-	for i in range(1):
-		date = j_data["data"]["forecast"][i]["date"]
-		high = j_data["data"]["forecast"][i]["high"]
-		fengli = j_data["data"]["forecast"][i]["fengli"]
-		
-		low = j_data["data"]["forecast"][i]["low"]
-		fengxiang = j_data["data"]["forecast"][i]["fengxiang"]
-		type = j_data["data"]["forecast"][i]["type"]
-		
-	fengli = fengli.replace('<![CDATA[','').replace(']]>','')
-	print city,aqi,ganmao,wendu,date,high,fengli,low,fengxiang,type
-	desc = u'[愉快]宝贝早安[愉快]'+'\n'+u'【'+date+city+u'天气预报】'+type+'\n'+u'【空气指数】%s'%aqi+'\n'+u'【温度】'+u'实时%s℃，'%wendu+u'最'+high+'，'+u'最'+low+'\n'+u'【风向/风力】'+fengxiang+fengli+'\n'+u'【感冒指数】'+ganmao
-	
-	return desc
-def  main():
-	#登陆微信
-	itchat.auto_login(hotReload=True)
-	#发送天气预报
-	while True:	
-		time.sleep(1)
-		current_time = time.localtime(time.time())
-		print "sended weather forecast..."+str(current_time.tm_sec)
-		if((current_time.tm_hour == 7) and (current_time.tm_min == 30) and (current_time.tm_sec == 0)):
-		#if current_time.tm_sec == 1:		
-			#给自己发
-			user_content = itchat.search_friends(name=u'雨一直下')
-			userName = user_content[0]['UserName']
-			itchat.send(getWeather(000),toUserName = userName)#
-			itchat.send(getWeather(000),toUserName = userName)#
-	
-			
-			#给宝贝发
-			user_content_baby = itchat.search_friends(name=u'')
-			userName_baby = user_content_baby[0]['UserName']
-			itchat.send(getWeather(0000),toUserName = userName_baby)#
-			#itchat.send(getWeather(0000),toUserName = userName_baby)#
-			
-
-if __name__ == "__main__":
-	main()
-
-
-
-'''
-	
