d87a2a40d1a42564f5dc3dd25ea74fc6f6e3900c
二营长 <admin@techseo.cn>
2018-04-19 15:07:40 +0800

    Update go_list.py

--- jd.com/pcbaby/go_list.py
+++ jd.com/pcbaby/go_list.py
@@ -6,57 +6,58 @@ from bs4 import BeautifulSoup
 reload(sys) 
 sys.setdefaultencoding('utf8')
 
-file_name = 'yuqian'
-
+def get_long_url(url):
+	try:
+		r = requests.get('https:'+url,headers=HeaderData.get_header(),timeout=60)				
+		s = BeautifulSoup(r.content,"lxml")
+		ptag = s.find_all("p",attrs={"class":"view mb20"})			
+		atag = s.find_all("a",attrs={"class":"careful"})
+		
+		for a in atag:
+			uri = a.get('href')
+			if 'qzbd' in uri:
+				get_long_url(uri)				
+			else:
+				f.write(uri+'\n')
+				print uri
+				
+		for p in ptag:
+			uri =  p.find('a').get('href')
+			if 'qzbd' in uri:
+				get_long_url(uri)
+			else:
+				f.write(uri+'\n')	
+				print uri
+				
+	except Exception,e:
+		print e	
+		
 def main():	
-	i = 0
-	for line in open('%s_detail_url.txt'%file_name):
-		url = 'https:'+line.strip()
-		try:
-			r = requests.get(url,headers=HeaderData.get_header(),timeout=60)				
-			s = BeautifulSoup(r.content,"lxml")
-			ptag = s.find_all("p",attrs={"class":"view mb20"})
-			
-			atag = s.find_all("a",attrs={"class":"careful"})
-			for a in atag:
-				url = a.get('href')
-				if 'qzbd' in url:
-					with open('yuqian_detail_url_qzbd.txt',r'a+') as my:
-						my.write(url+'\n')
-				else:
-					f.write(url+'\n')
-					
-			for p in ptag:
-				url =  p.find('a').get('href')
-				if 'qzbd' in url:
-					with open('yuqian_detail_url_qzbd.txt',r'a+') as my:
-						my.write(url+'\n')
-				else:
-					f.write(url+'\n')				
-		except Exception,e:
-			print e	
-		i += 1
-		print i,line.strip()
-	
-	'''
-	for i in range(1,2):
-		url = 'https://baike.pcbaby.com.cn/yunqian.html'
+
+	url_list = ['http://baike.pcbaby.com.cn/fenmian.html','http://baike.pcbaby.com.cn/yuezi.html','http://baike.pcbaby.com.cn/xinshenger.html','http://baike.pcbaby.com.cn/yinger.html','http://baike.pcbaby.com.cn/youer.html','http://baike.pcbaby.com.cn/xuelingqian.html','http://baike.pcbaby.com.cn/yongpin.html','http://baike.pcbaby.com.cn/shenghuo.html']
+	for i in url_list:	
+		url = i
+		print url
 		try:
 			r = requests.get(url,headers=HeaderData.get_header(),timeout=60)				
 			s = BeautifulSoup(r.content,"lxml")
 			atag = s.find("div",attrs={"id":"Jbaike"}).find_all('a')			
 			for a in atag:
 				url =  a.get('href')
-				if 'qzbd' in url:
-					f.write(url+'\n')
-				print i
+				if 'qzbd' in url:					
+					f_qzbd.write(url+'\n')
+					get_long_url(url)				
+			
 		except Exception,e:
 			print e			
 		time.sleep(0.1)	
-	'''
+	
 		
 if __name__ == '__main__':
-	f = open('%s_long_url.txt'%file_name,r'a+')
+	f_qzbd = open('qzbd_url.txt',r'a+')
+	f = open('long_url.txt',r'a+')
 	main()
 	f.close()
+	f_qzbd.close()
+	
 	
