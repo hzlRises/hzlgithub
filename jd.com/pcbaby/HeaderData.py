854baa846b1ecffd5768724c29d6611e875c97ad
二营长 <admin@techseo.cn>
2018-04-19 11:01:50 +0800

    Create HeaderData.py

---
+++ jd.com/pcbaby/HeaderData.py
@@ -0,0 +1,24 @@
+#coding:utf-8
+def get_header():
+	headers = {
+		":authority": "baike.pcbaby.com.cn",
+		":method": "GET",
+		":path": "/shenghuo.html",
+		":scheme": "https",
+		"accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8",
+		"accept-encoding": "gzip, deflate, br",
+		"accept-language": "zh-CN,zh;q=0.9,en;q=0.8",
+		"cache-control": "no-cache",
+		"cookie": '''u4ad=271lhtu0h; pcsuv=1521013529933.a.897010909; u=272bjzia; c=27365whf; UM_distinctid=16285c580fb14c-0c370e710fc171-4446062d-100200-16285c580fc1e5; JSESSIONID=abcsLhnIvC5eXgEog0Blw; captcha=f0cd3e1829426a5e-4f748848162dbc42a99-24e9-1317960486459834; pcuvdata=lastAccessTime=1524106512557|visits=6; channel=3955''',
+		"pragma": "no-cache",
+		"upgrade-insecure-requests": "1",
+		"user-agent": "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.181 Safari/537.36}",
+		}
+	return headers
+def main():
+	pass
+if __name__ == '__main__':
+	main()
+	
+	
+	
