35788ad4283527dde42bf8745774f11027c60f2e
二营长 <admin@techseo.cn>
2018-04-23 22:28:02 +0800

    Create HeaderData.py

---
+++ jd.com/mama/HeaderData.py
@@ -0,0 +1,20 @@
+#coding:utf-8
+def get_header():
+	headers = {
+		"Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8",
+		"Accept-Encoding": "gzip, deflate",
+		"Accept-Language": "zh-CN,zh;q=0.9,en;q=0.8",
+		"Cache-Control": "max-age=0",
+		"Connection": "keep-alive",
+		"Cookie": '''step=6; step=5; mamastatid=vlstat-1524403670000-301379933; cookie_map=35cd82e4639236a53c4a94384bc55f6a; PPSID=va00nl3tkv88si8n2a7d3pijm2; 9wni_2132_saltkey=4NI32FId; 9wni_2132_lastvisit=1524429351; bdshare_firstime=1524404159376; UM_distinctid=162ed968ee11b4-0e59d7fef79a7d-51693374-1fa400-162ed968ee210c; CNZZDATA1000231791=1421474513-1524404487-https%253A%252F%252Fwww.mama.cn%252F%7C1524404487; 9wni_2132_sid=9OjFDF; 9wni_2132_lastact=1524433290%09index.php%09home; newGuideIndex=1; PHPSESSID=shpic6bhuosbii8mnkplsee9c3; Hm_lvt_f2babe867b10ece0ff53079ad6c04981=1524403671,1524488864; mmt_step=true; step=1; MAMADATA57=mama_rp=0&si=3ffa2613c5a82b62dd980610f5db5ad4&ltime=1524488889088&rtime=1&sinfid=0&sinpage=cms_home_index&location=https%3A%2F%2Fwww.mama.cn%2F; step=2; zbd1_0=1; MAMADATA58=mama_rp=3&si=8c7ac236e9c089a1095647e1bde73eb3&ltime=1524488908656&rtime=1&sinfid=0&sinpage=pc_Question_Question&location=https%3A%2F%2Fwww.mama.cn%2Fask%2Fq13616995-p1.html; MAMADATA615=mama_rp=35&si=8c7ac236e9c089a1095647e1bde73eb3&ltime=1524489152548&rtime=1&sinfid=0&sinpage=__&location=http%3A%2F%2Fwww.mama.cn%2Fz%2Ft20001%2F; Hm_lpvt_f2babe867b10ece0ff53079ad6c04981=1524489153''',
+		"Host": "www.mama.cn",
+		"Referer": "http://www.mama.cn/z/t665/",
+		"Upgrade-Insecure-Requests": "1",
+		"User-Agent": "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.181 Safari/537.36",
+	}
+	return headers
+def main():
+	pass
+if __name__ == '__main__':
+	main()
+	
