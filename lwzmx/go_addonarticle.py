508e50260f671622a7f88922c3dec9269e7f2500
二营长 <admin@techseo.cn>
2018-04-25 22:30:00 +0800

    Update go_addonarticle.py

--- lwzmx/go_addonarticle.py
+++ lwzmx/go_addonarticle.py
@@ -4,7 +4,7 @@ import xlrd
 import xlwt,MySQLdb
 
 
-data = xlrd.open_workbook('lwzxm_ixinwei.xlsx')
+data = xlrd.open_workbook('lwzmx_addonarcticle.xlsx')
 t1 = data.sheets()[0]#第1张表
 
 rows = t1.nrows#获取行数
@@ -14,16 +14,13 @@ conn = MySQLdb.connect('','','','',charset='utf8')#连接
 
 with conn:
 	cur = conn.cursor()#让python获得执行sql的权限
-	num = 0
-	for i in range(0,rows):
-		try:			
-			sql = 'insert into dede_addonarticle (aid,typeid,body,redirecturl,templet,userip) values("%s","%s","%s","%s","%s","%s")' %(t1.row_values(i)[0],t1.row_values(i)[1],t1.row_values(i)[2],t1.row_values(i)[3],t1.row_values(i)[4],t1.row_values(i)[5])#要执行sql语句
+	for i in range(1,rows):
+		try:
+			sql = '''insert into dede_addonarticle (aid,typeid,body,redirecturl,templet,userip) values('%s','%s','%s','%s','%s','%s')''' %(t1.row_values(i)[0],t1.row_values(i)[1],t1.row_values(i)[2],t1.row_values(i)[3],t1.row_values(i)[4],t1.row_values(i)[5])#要执行sql语句
 			cur.execute(sql)#执行
 			conn.commit()#提交
 		except Exception,e:
-			print e
-		num += 1
-		print 2
-		print num
+			print e	
+		print i
 		
 conn.close()#关闭
