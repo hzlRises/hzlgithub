dd311d572f965a0e4d5a1fa2dd1f259ddf1ba36c
二营长 <admin@techseo.cn>
2018-05-14 12:22:52 +0800

    Update go.py

--- weixin/go.py
+++ weixin/go.py
@@ -91,8 +91,48 @@ def text_reply(msg):
 		'''
 		user_content = itchat.search_friends(name=u'雨一直下')
 		userName = user_content[0]['UserName']
-		itchat.send_image('send_%s.png'%index,toUserName = userName)
+		itchat.send_image('send_%s.png'%index,toUserName = userName)		
 		'''
+		
+		
+	elif '我的基金' in msg['Text']:
+		fund_list = ['162605','160505','002121','000011','163402','070032','217027','165312','519066','110022']
+		for index,fund in enumerate(fund_list):
+			fund_id = str(fund)		
+			url = 'https://www.baidu.com/s?ie=UTF-8&wd=%s'%str(fund_id)		
+			picName = 'fund_%s.png'%index
+			browser = webdriver.PhantomJS(executable_path=r'D:\programfiles\anaconda\Lib\site-packages\selenium\webdriver\phantomjs\bin\phantomjs.exe')
+			browser.get(url)
+			browser.maximize_window()
+			browser.save_screenshot(picName)#保存截图	
+			
+			
+			imgelement = browser.find_element_by_xpath('//*[@id="1"]')
+			'''
+			if fund_id == '002121':
+				imgelement = browser.find_element_by_xpath('//*[@id="2"]')
+			'''
+			location = imgelement.location#获取x,y轴坐标
+			size = imgelement.size#获取长宽
+			
+			rangle = (int(location['x']),int(location['y']),int(location['x']+size['width']),int(location['y']+size['height']-20))
+			i = Image.open(picName)	#打开
+			tinaqi = i.crop(rangle)#使用Image的crop函数，从截图中再次截取我们需要的		
+			tinaqi.save('send_fund_%s.png'%index)		
+			browser.close()	
+			
+			
+			#发送
+			user_content = itchat.search_friends(name=u'雨一直下')
+			userName = user_content[0]['UserName']
+			itchat.send_image('send_fund_%s.png'%index,toUserName = userName)
+			
+			'''
+			if fund_id in ['070032','217027','165312','519066','110022']:
+				user_content_baby = itchat.search_friends(name=u'徐莹')
+				userName_baby = user_content_baby[0]['UserName']
+				itchat.send_image('send_fund_%s.png'%index,toUserName = userName_baby)		
+			'''
 		
 	else:
 		url = 'http://yhq.techseo.cn/yhq/?r=l&kw=%s'%(urllib.quote(msg['Text'].encode("utf-8")))
