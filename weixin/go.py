6acd9958d75925cce62784a1180e3d256d9a5cec
二营长 <admin@techseo.cn>
2018-06-13 09:28:26 +0800

    Update go.py

--- weixin/go.py
+++ weixin/go.py
@@ -31,7 +31,7 @@ def text_reply(msg):
 		click_url = jd.getFanliLink(sku_id)		
 		message = u'一一一一返 利 信 息一一一一\n'+goods_name+'\n'+u'【商品原价】'+price+'元'+'\n'+u'【商品返利】'+fanli+'元'+'\n'+u'【返利链接】'+jd.getShortUrl(click_url)
 	elif '帮助' in msg['Text']:
-		message = '[愉快]【在淘宝购物前领券】假如你想买鼠标垫，直接把鼠标垫三个字发给机器人，机器人会把找到的鼠标垫相关商品链接发给你，点进去复制淘口令，再打开淘宝/天猫APP就可以领到优惠券啦。\n[愉快]【在京东购物】打开京东APP，找到自己想买的商品，点击右上角分享按钮，把商品链接复制，发给我就可以看到返利链接啦，确认收货后发送【提现】，等待客服审核就可以啦。\n快试试吧...'
+		message = '[愉快]【淘宝购物领券】假如你想买鼠标垫，请发送"找鼠标垫"给机器人，机器人会把找到的鼠标垫相关优惠券链接发给你，点进去复制淘口令，再打开淘宝/天猫APP就可以领到优惠券啦。\n[愉快]【京东购物返利】打开京东APP，找到自己想买的商品，点击右上角分享按钮，把商品分享给机器人，就可以看到返利链接啦，确认收货后发送【提现】，等待客服审核就可以啦。\n快试试吧...'
 	#回复表情
 	elif '[' in msg['Text'] and ']' in msg['Text']:
 		message = u'[愉快]'
@@ -46,11 +46,11 @@ def text_reply(msg):
 		itchat.send( friend_name+u'申请提现',toUserName = userName)				
 		#判断提现人性别
 		if friend_content['Sex'] == 1:
-			message = friend_name +u'帅哥，请耐心等待哦，由于人数较多，客服火速确认中呢...'		
+			message = friend_name +u'帅哥请耐心等待哦，由于人数较多，客服火速确认中'		
 		elif friend_content['Sex'] == 0:
-			message = friend_name +u'美女，请耐心等待哦，由于人数较多，客服火速确认中呢...'
+			message = friend_name +u'美女请耐心等待哦，由于人数较多，客服火速确认中'
 		else:
-			message = friend_name +u'请耐心等待哦，由于人数较多，客服火速确认中呢...'			
+			message = friend_name +u'请耐心等待哦，由于人数较多，客服火速确认中'			
 	
 	#活动页转链接
 	elif 'hzlxy' in msg['Text']:
@@ -132,14 +132,17 @@ def text_reply(msg):
 			if fund_id in ['070032','217027','165312','519066','110022']:
 				user_content_baby = itchat.search_friends(name=u'徐莹')
 				userName_baby = user_content_baby[0]['UserName']
-				itchat.send_image('send_fund_%s.png'%index,toUserName = userName_baby)		
+				itchat.send_image('send_fund_%s.png'%index,toUserName = userName_baby)					
 			'''
-		
-	else:
+	elif '找' in msg['Text']:
+		msg['Text'] = msg['Text'].replace('找','')
 		url = 'http://techseo.cn/yhq/?r=l&kw=%s'%(urllib.quote(msg['Text'].encode("utf-8")))
-		message = u'一一一一导 购 信 息一一一一\n已为您找到:%s\n点击下方链接查看\n%s\n-----------\n发送【帮助】查看使用机器人流程\n更多大额神券商品点击下方链接：\nhttp://t.cn/RYaBrUa'%(msg['Text'],jd.getShortUrl(url))
+		message = u'一一一一导 购 信 息一一一一\n已为您找到：%s\n点击下方链接查看\n%s\n-----------\n发送【帮助】查看使用机器人流程\n更多大额神券商品点击下方链接：\nhttp://t.cn/RYaBrUa'%(msg['Text'],jd.getShortUrl(url))
 
-	
+	else:
+		#url = 'http://techseo.cn/yhq/?r=l&kw=%s'%(urllib.quote(msg['Text'].encode("utf-8")))
+		#message = u'一一一一导 购 信 息一一一一\n已为您找到:%s\n点击下方链接查看\n%s\n-----------\n发送【帮助】查看使用机器人流程\n更多大额神券商品点击下方链接：\nhttp://t.cn/RYaBrUa'%(msg['Text'],jd.getShortUrl(url))
+		message = '您输入的指令有误，请发送【帮助】查看教程'
 	print message
 	itchat.send(message,msg.fromUserName)
 	
