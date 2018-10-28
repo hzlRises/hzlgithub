251daaf04863d1c84d09e79c75650d3e954bd0d8
二营长 <admin@techseo.cn>
2018-10-28 18:21:38 +0800

    Update weather.py

--- weixin/weather.py
+++ weixin/weather.py
@@ -14,13 +14,13 @@ def save_jpg():
 		encode_city = quote(city)
 		url = 'https://www.baidu.com/s?ie=UTF-8&wd=%s'%encode_city	
 		picName = '%s.png'%index		
-		browser = webdriver.PhantomJS(executable_path=r'D:\programfiles\anaconda\Lib\site-packages\selenium\webdriver\phantomjs\bin\phantomjs.exe')
+		browser = webdriver.PhantomJS(executable_path=r'/usr/local/bin/phantomjs')
 		browser.get(url)
 		browser.maximize_window()
 		browser.save_screenshot(picName)#保存截图		
 		
 		#获取天气模板的位置、尺寸大小
-		imgelement = browser.find_element_by_xpath('//*[@id="1"]')
+		imgelement = browser.find_element_by_xpath('//*[@id="1"]/div[1]')
 		location = imgelement.location#获取天气x,y轴坐标
 		size = imgelement.size#获取天气的长宽
 		
@@ -57,20 +57,21 @@ def get_fund():
 	
 	'''
 	#fund_list = ['110022','160505','163402','162605','002121','000011','070032','217027','165312','519066','110022']
-	fund_list = ['162605','160505','002121','000011','163402','070032','217027','165312','519066','110022']
+	#fund_list = ['162605','160505','002121','000011','163402','070032','217027','165312','519066','110022']
+	fund_list = ['dapan',]
 	
 	for index,fund in enumerate(fund_list):
 		fund_id = str(fund)		
-		#url = 'https://www.baidu.com/s?ie=UTF-8&wd=%s'%str(fund_id)
-		url = 'http://fund.eastmoney.com/%s.html'%str(fund_id)			
+		url = 'https://www.baidu.com/s?ie=UTF-8&wd=%s'%str(fund_id)
+		#url = 'http://fund.eastmoney.com/%s.html'%str(fund_id)			
 		picName = 'fund_%s.png'%index
-		browser = webdriver.PhantomJS(executable_path=r'D:\programfiles\anaconda\Lib\site-packages\selenium\webdriver\phantomjs\bin\phantomjs.exe')
+		browser = webdriver.PhantomJS(executable_path=r'/usr/local/bin/phantomjs')
 		browser.get(url)
 		browser.maximize_window()
 		browser.save_screenshot(picName)#保存截图	
 		
 		
-		imgelement = browser.find_element_by_xpath('//*[@id="body"]/div[14]/div/div/div[1]')
+		imgelement = browser.find_element_by_xpath('//*[@id="1"]/div[1]')
 		'''
 		if fund_id == '002121':
 			imgelement = browser.find_element_by_xpath('//*[@id="2"]')
@@ -91,10 +92,10 @@ def get_fund():
 		itchat.send_image('send_fund_%s.png'%index,toUserName = userName)
 		
 		
-		if fund_id in ['070032','217027','165312','519066','110022']:
-			user_content_baby = itchat.search_friends(name=u'徐莹')
-			userName_baby = user_content_baby[0]['UserName']
-			itchat.send_image('send_fund_%s.png'%index,toUserName = userName_baby)		
+		
+		user_content_baby = itchat.search_friends(name=u'徐莹')
+		userName_baby = user_content_baby[0]['UserName']
+		itchat.send_image('send_fund_%s.png'%index+1,toUserName = userName_baby)		
 		
 		
 	
@@ -106,7 +107,7 @@ def main():
 		print time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time()))		
 		# get_fund()
 		# break		
-		if ((current_time.tm_hour == 7) and (current_time.tm_min) == 0 and (current_time.tm_sec == 0)):
+		if ((current_time.tm_hour == 6) and (current_time.tm_min) == 29 and (current_time.tm_sec == 0)):
 			save_jpg()
 		if(((current_time.tm_hour == 14) and (current_time.tm_min) == 30 and (current_time.tm_sec == 0))):
 			get_fund()
